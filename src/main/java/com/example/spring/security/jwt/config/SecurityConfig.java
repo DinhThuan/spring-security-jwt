e6078dafeae83c4b0fdfd87312b88e7584d1af65
Dinh Thuan <dvthuan1@cmc.com.vn>
2022-04-06 16:41:38 +0700

    config authentication

--- src/main/java/com/example/spring/security/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/jwt/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -32,18 +33,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        //Disable crsf url /rest/**
+//        http.csrf().ignoringAntMatchers("/rest/api/**");
+//
+////        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+//        http.authorizeRequests().antMatchers("/api/v1/auth/login").permitAll();
+//
+//        http.antMatcher("/api/v1/**").httpBasic()
+//                .authenticationEntryPoint(restServiceEntryPoint())
+////                .and()
+////                .authorizeRequests()
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+////        super.configure(http);
 
-//        http.authorizeRequests().antMatchers(HttpMethod.GET, "/**").permitAll();
-//        http.authorizeRequests().antMatchers(HttpMethod.POST, "/**").permitAll();
-//        http.authorizeRequests().antMatchers("/**").permitAll().and().httpBasic();
-//        http.csrf().ignoringAntMatchers("/api/v1/**");
-        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
-        http.antMatcher("/api/v1/**").httpBasic()
-                .authenticationEntryPoint(restServiceEntryPoint())
+
+        http
+                .cors()
                 .and()
-                .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
-//        super.configure(http);
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restServiceEntryPoint()) //Ném ngoại lệ
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers()
+                .permitAll()
+                .antMatchers("/api/v1/auth/login") //Đường dẫn /api/login-admin sẽ được truy cập bình thường mà ko cần check
+                .permitAll()
+                .anyRequest()
+                .authenticated(); //Mọi đường dẫn còn lại yêu cầu gửi Authentication String trên header để check.
 
     }
 }
