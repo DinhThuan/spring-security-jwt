256793b21a2b50c9ef0bb655c05aca4bb23747f4
Dinh Thuan <dvthuan1@cmc.com.vn>
2022-04-13 11:25:18 +0700

    update code

--- src/main/java/com/example/spring/security/jwt/authentication/AuthenticationController.java
+++ src/main/java/com/example/spring/security/jwt/authentication/AuthenticationController.java
@@ -3,19 +3,16 @@ package com.example.spring.security.jwt.authentication;
 import com.example.spring.security.jwt.constant.MessageConstants;
 import com.example.spring.security.jwt.response.JwtResponse;
 import com.example.spring.security.jwt.service.JwtService;
+import com.example.spring.security.jwt.service.RefreshTokenService;
 import com.nimbusds.jose.JOSEException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
+import java.text.ParseException;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin("http://localhost:8080")
 //@RestController
@@ -26,18 +23,22 @@ public class AuthenticationController {
     private JwtService jwtService;
     @Autowired
     private AuthenticationService authenticationService;
+    @Autowired
+    private RefreshTokenService refreshTokenService;
 
     @PostMapping("login")
-    public ResponseEntity<JwtResponse> singIn(@RequestBody UserLogin user) throws JOSEException {
+    public ResponseEntity<JwtResponse> singIn(@RequestBody UserLogin user) throws JOSEException, ParseException {
         JwtResponse jwtResponse = new JwtResponse();
         HttpStatus httpStatus = null;
         if (authenticationService.checkLogin(user)) {
             String[] roles = authenticationService.getRoleByUser(user);
             jwtResponse.setRoles(Arrays.asList(roles));
-            jwtResponse.setAccessToken(jwtService.generateTokenLogin(user.getUsername()));
-            jwtResponse.setExpiredAccessToken(new Date());
+            String token  = jwtService.generateTokenLogin(user.getUsername());
+            jwtResponse.setAccessToken(token);
+            jwtResponse.setExpiredAccessToken(jwtService.getExpirationDateFromToken(token));
             jwtResponse.setType(MessageConstants.TYPE_TOKEN);
 //            jwtResponse.setRoles(roles);
+            jwtResponse.setRefreshToken(refreshTokenService.createRefreshToken());
             httpStatus = HttpStatus.OK;
         } else {
             jwtResponse.setMessage("Wrong user or password");
@@ -45,4 +46,18 @@ public class AuthenticationController {
         }
         return new ResponseEntity<>(jwtResponse, httpStatus);
     }
+
+//    @PostMapping("/refreshtoken")
+//    public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
+//        String requestRefreshToken = request.getRefreshToken();
+//        return refreshTokenService.findByToken(requestRefreshToken)
+//                .map(refreshTokenService::verifyExpiration)
+//                .map(RefreshToken::getUser)
+//                .map(user -> {
+//                    String token = jwtUtils.generateTokenFromUsername(user.getUsername());
+//                    return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
+//                })
+//                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
+//                        "Refresh token is not in database!"));
+//    }
 }
